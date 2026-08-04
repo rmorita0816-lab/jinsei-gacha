1fe3b52a7fde2a50f97c6b23c2e06e71b29f1983
rmorita0816-lab <r.morita0816@gmail.com>
2026-08-05 08:16:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -72,13 +72,25 @@ if(luck === "GOD"){
         navigator.vibrate([300,150,300]);
     }
 }
-  result.innerHTML = `
-    <p>👶 出身：${random(birthplace)}</p>
-    <p>💼 職業：${random(jobs)}</p>
-    <p>💰 年収：${random(incomes)}</p>
-    <p>💕 恋愛：${random(loves)}</p>
-    <h2 class="${className}">🍀 ${luck}</h2>
-  `;
+result.innerHTML = `
+<p>👶 出身：<span id="birth"></span></p>
+<p>💼 職業：<span id="job"></span></p>
+<p>💰 年収：<span id="income"></span></p>
+<p>💕 恋愛：<span id="love"></span></p>
+<h2 class="${className}">🍀 <span id="luck"></span></h2>
+`;
+
+const birth = document.getElementById("birth");
+const job = document.getElementById("job");
+const income = document.getElementById("income");
+const love = document.getElementById("love");
+const luckText = document.getElementById("luck");
+
+birth.textContent = await slotEffect(birth, birthplace);
+job.textContent = await slotEffect(job, jobs);
+income.textContent = await slotEffect(income, incomes);
+love.textContent = await slotEffect(love, loves);
+luckText.textContent = luck;
 
   button.disabled = false;
 });
